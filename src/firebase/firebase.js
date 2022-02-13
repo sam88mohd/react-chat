adc1b61f77e1a801a0045c3f310c0fa01150e5ed
adminhtlsam <adminhtlsam@pop-os.localdomain>
2022-02-13 12:05:41 +0800

    firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDkUoQl3vID8MZR-lOp0jSjiKJ4GkZq0Oo",
+  authDomain: "react-chat-red.firebaseapp.com",
+  projectId: "react-chat-red",
+  storageBucket: "react-chat-red.appspot.com",
+  messagingSenderId: "522914289583",
+  appId: "1:522914289583:web:47422516fc7d8bb67b415a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
